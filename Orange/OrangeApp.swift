1f35463f24a08ddcc2a7289dafdaebc9e9c5f21d
Leon Rinkel <leon@rinkel.me>
2023-06-18 12:43:36 +0200

    Initial Commit

---
+++ Orange/OrangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrangeApp.swift
+//  Orange
+//
+//  Created by Leon Rinkel on 18.06.23.
+//
+
+import SwiftUI
+
+@main
+struct OrangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
