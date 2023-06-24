d2091fe118059ddb09dccbfce5b413f6e4d6ad7a
Leon Rinkel <leon@rinkel.me>
2023-06-24 14:03:08 +0200

    Add story context menu

--- Orange/Views/StoryRowView.swift
+++ Orange/Views/StoryRowView.swift
@@ -126,6 +126,46 @@ struct StoryRowView: View {
                 }
             }
         }
+        .contextMenu {
+            if let story = storyProvider.story, let parsedUrl = story.parsedURL {
+                Link(destination: parsedUrl) {
+                    Label("Open in Safari", systemImage: "safari")
+                }
+            }
+            if let savedStory {
+                Button {
+                    viewContext.delete(savedStory)
+                    self.savedStory = nil
+                    do {
+                        try viewContext.save()
+                    } catch {
+                        // TODO: Replace this implementation with code to handle the error appropriately.
+                        // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                        let nsError = error as NSError
+                        fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+                    }
+                } label: {
+                    Label("Unsave", systemImage: "bookmark.slash")
+                }
+            }
+            else {
+                Button {
+                    if let story = storyProvider.story {
+                        savedStory = SavedStory(context: viewContext, from: story)
+                        do {
+                            try viewContext.save()
+                        } catch {
+                            // TODO: Replace this implementation with code to handle the error appropriately.
+                            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                            let nsError = error as NSError
+                            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+                        }
+                    }
+                } label: {
+                    Label("Save", systemImage: "bookmark")
+                }
+            }
+        }
     }
 }
 
